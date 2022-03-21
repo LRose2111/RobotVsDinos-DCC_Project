ed99624c4e1a1a453682655461a48d4cdd0a16c6
Lakota Rose <itsrosebro@gmail.com>
2022-03-21 15:09:09 -0500

    Fixed typos in Dinosaur.py file, and moved a list from dinosaur.py to herd.py.

--- dinosaur.py
+++ dinosaur.py
@@ -1,11 +1,8 @@
-dino_list = ['']
 class Dinosaur:
     def __init__(self, name):
         self.dinosaur_name = name
         self.dinosaur_health = None
         self.attackpower = None
 
-    
-
     def dino_attack(self, robo):
-        print(f'{self.dinosaur_name} attacks {robo.name} with his chompers!}')
+        print(f'{self.dinosaur_name} attacks {robo.name} with his chompers!')
--- herd.py
+++ herd.py
@@ -1,4 +1,5 @@
 from dinosaur import Dinosaur
+dino_list = ['']
 class Herd:
     def __init__(self):
         self.herd = None
